7fc85117918c58f352cdbf74142a6a9bb6b0f102
GreybeardGM <webmaster@sector-001.de>
2025-06-07 10:29:04 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,10 +1,13 @@
 import { HirelingSheet } from "./hireling-sheet.js";
 
 Hooks.once("init", () => {
-  console.log("Registering HirelingSheet for NPC actors");
+  console.log("✅ Hireling Module: INIT");
+
   Actors.registerSheet("dungeonworld-hirelings", HirelingSheet, {
     types: ["npc"],
     label: "Hireling Sheet",
-    makeDefault: false,
+    makeDefault: false
   });
+
+  console.log("✅ HirelingSheet registered");
 });
