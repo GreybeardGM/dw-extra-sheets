bf8d853e11b659e4e70a8cfe442459de010cec75
GreybeardGM <webmaster@sector-001.de>
2025-06-07 09:27:37 +0200

    Delete scripts/module.js

--- scripts/module.js
+++
@@ -1,9 +0,0 @@
-import { HirelingSheet } from "./hireling-sheet.js";
-
-Hooks.once("init", () => {
-  Actors.registerSheet("dungeonworld-hirelings", HirelingSheet, {
-    types: ["npc"],
-    label: "Hireling Sheet",
-    makeDefault: false
-  });
-});
