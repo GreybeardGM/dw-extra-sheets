5163d4d79d13764e5ba4d992c37cc8036e5a5dd3
GreybeardGM <webmaster@sector-001.de>
2025-06-13 20:10:39 +0200

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -1,17 +1,21 @@
-async _prepareItems(sheetData) {
-  const equipment = [];
-  // Use this.actor.items instead of sheetData.items
-  const itemsArr = this.actor.items ? Array.from(this.actor.items) : [];
-  for (let i of itemsArr) {
-    if (i.system?.description) {
-      i.system.descriptionEnriched = await TextEditor.enrichHTML(i.system.description, {
+// items.js
+
+/**
+ * Prepare/enrich items (add .descriptionEnriched, etc) for a given array.
+ * @param {Array} items - The items to process.
+ * @param {Actor|Object} actor - The actor, for ownership/rollData.
+ * @returns {Promise<Array>} The enriched items (can be used in-place).
+ */
+export async function prepareItems(items, actor) {
+  for (let item of items) {
+    if (item.system?.description) {
+      item.system.descriptionEnriched = await TextEditor.enrichHTML(item.system.description, {
         async: true,
         documents: true,
-        secrets: this.actor.isOwner,
-        rollData: this.actor.getRollData(),
+        secrets: actor.isOwner,
+        rollData: actor.getRollData(),
       });
     }
-    if (i.type === "equipment") equipment.push(i);
   }
-  sheetData.equipment = equipment;
+  return items;
 }
