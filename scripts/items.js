3c717dbb25312f1f04ee3d9560904bf2de00ca48
GreybeardGM <webmaster@sector-001.de>
2025-06-16 11:05:56 +0200

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -2,22 +2,38 @@
 
 /**
  * Prepare/enrich items (add .descriptionEnriched, etc) for a given array.
+ * @param {context|Object} context from the getData to manipulate.
  * @param {Actor|Object} actor - The actor, for ownership/rollData.
- * @returns {Promise<Array>} The enriched items (can be used in-place).
  */
-export async function prepareEquipmentItems(actor) {
-  const equipment = [];
-  const itemsArr = actor.items ? Array.from(actor.items) : [];
-  for (let i of itemsArr) {
-    if (i.system?.description) {
-      i.system.descriptionEnriched = await TextEditor.enrichHTML(i.system.description, {
-        async: true,
-        documents: true,
-        secrets: actor.isOwner,
-        rollData: actor.getRollData(),
-      });
-    }
-    if (i.type === "equipment") equipment.push(i);
+export async function prepareEquipmentItems(sheetData, actor) {
+  // Tag stringification, if needed
+  if (sheetData.system.tags != undefined && sheetData.system.tags != '') {
+    let tagArray = [];
+    try { tagArray = JSON.parse(sheetData.system.tags); }
+    catch (e) { tagArray = [sheetData.system.tags]; }
+    sheetData.system.tagsString = tagArray.map(item => item.value).join(', ');
+  } else {
+    sheetData.system.tags = sheetData.system.tagsString;
   }
-  return equipment;
+
+  const enrichmentOptions = {
+    async: true,
+    documents: true,
+    secrets: actor.isOwner,
+    rollData: actor.getRollData(),
+  };
+
+  const equipment = [];
+  for (let i of sheetData.items) {
+    const item = actor.items.get(i._id);
+    enrichmentOptions.relativeTo = item;
+    enrichmentOptions.rollData = item.getRollData();
+    if (i.system?.description) {
+      i.system.descriptionEnriched = await TextEditor.enrichHTML(i.system.description, enrichmentOptions);
+    }
+    i.img = i.img || DEFAULT_TOKEN;
+    if (i.type === 'equipment') equipment.push(i);
+  }
+
+  sheetData.equipment = equipment;
 }
